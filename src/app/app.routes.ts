f12a18562e1555f4ef0736e12000279627c393d8
kubilay-fh <kubilaysabah@gmail.com>
2024-08-07 19:17:30 +0300

    add products module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from '@modules/products/products.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ProductsComponent
+  }
+];
---
+++ src/app/modules/products/products.component.ts
@@ -0,0 +1,53 @@
+
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Subject, takeUntil } from "rxjs";
+
+import { DataViewModule } from 'primeng/dataview';
+import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
+
+import { Product } from '@domain/product';
+import ProductService from '@services/product.service';
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [
+    CommonModule,
+    DataViewModule,
+    ButtonModule,
+    TagModule,
+  ],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.scss',
+  providers: [ProductService]
+})
+export class ProductsComponent implements OnInit, OnDestroy {
+  private destroy$: Subject<void> = new Subject<void>();
+
+  products: Product[] = [];
+
+  constructor(
+    private productService: ProductService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.productService.list()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (products) => {
+          this.products = products;
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
